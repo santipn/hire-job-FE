b5b9e764f4490a0f77ea7e966e448490f92f28c0
Gyga Maulana <gygamaulana@gmail.com>
2022-08-09 01:19:00 +0700

    newest

--- src/layouts/chat/index.js
+++ src/layouts/chat/index.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import FooterLayout from '../../components/footer'
 import NavbarComponent from '../../components/navbar'
+import styles from './chat.module.scss'
 
 const ChatLayout = () => {
   return (
     <>
     <NavbarComponent/>
-    <div>ChatLayout</div>
+    <div className = "container">
+        <div className = "row clearfix">
+            <div className = "col-lg-12">
+                <div className={`${styles.chatApp} card`}>
+                    adasd
+                </div>
+            </div>
+        </div>
+    </div>
     <FooterLayout/>
     </>
   )
