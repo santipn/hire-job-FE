d5e93c781d1816691dada7043e0fd0d5f8d0a96a
muhammadsandi12 <muhammadsandimaulana12@gmail.com>
2022-08-11 04:47:40 +0700

    notif

--- src/layouts/notification/index.js
+++ src/layouts/notification/index.js
@@ -11,24 +11,36 @@ const fetcher = url => axios.get(url).then(res => res.data).catch(err => err.res
 const NotifLayout = () => {
     const { data: user } = useVerify({})
     const { data: token } = useSWR(`/api/auth/token`, fetcher)
-    console.log(token)
 
+    
+    useEffect(() => {
+      
+      const getData = async () =>{
+         await axios({
+              method: "GET",
+              url: 'http://localhost:3001/api/v1/notification',
+              headers: {
+                  Authorization : `Bearer ${token?.token}`
+              }
+          }).then((res) => {
+               console.log(res)
+          }).catch((err) => {console.log(err) })
+      }
+    }, [])
 
-    // useEffect({})
-
-return (<>
+    return (<>
         <NavbarComponent />
         <div className={style.container}>
             <div className={style['section-left']}>
                 <div className={style['title']}>
                     Notification
                 </div>
-                
+
                 <div className={style['wrapper-card']}>
                     <div>PT. MARTABAK  mengirim email, silahkan cek email!</div>
                 </div>
 
-              
+
             </div>
 
 
